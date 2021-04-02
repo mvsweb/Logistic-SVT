0ff0590016e55caa0fb2633d2aedbe980ae68354
Melvisharam info <63548063+mvsweb@users.noreply.github.com>
2021-04-02 10:53:11 +0000

    Updates

---
+++ static/js/18.ec0f9de7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[18],{972:function(e,t,i){"use strict";i.r(t);var c=i(1),n=i(20);t.default=function(e){return Object(c.useEffect)((function(){window.open("https://mybillbook.in/")}),[]),Object(n.jsx)("div",{})}}}]);
+//# sourceMappingURL=18.ec0f9de7.chunk.js.map
